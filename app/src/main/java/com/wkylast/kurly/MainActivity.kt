09fbdc0d278e986b06960f712b1617c0446571d0
유건우 <kw.yu@birdview.kr>
2025-05-28 11:57:05 +0900

    스카폴드 제거

--- app/src/main/java/com/wkylast/kurly/MainActivity.kt
+++ app/src/main/java/com/wkylast/kurly/MainActivity.kt
@@ -3,16 +3,11 @@ package com.wkylast.kurly
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Scaffold
-import androidx.compose.ui.Modifier
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-            }
         }
     }
 }
