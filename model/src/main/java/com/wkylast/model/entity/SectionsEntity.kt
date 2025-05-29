f09af764c61ef4cbc9cf4beee42fffc4d367ad19
유건우 <kw.yu@birdview.kr>
2025-05-29 14:56:43 +0900

    sections entity 추가

---
+++ model/src/main/java/com/wkylast/model/entity/SectionsEntity.kt
@@ -0,0 +1,15 @@
+package com.wkylast.model.entity
+
+import com.wkylast.model.type.UiType
+
+data class SectionsEntity(
+    val data: List<SectionEntity>,
+    val nextPage: Int?,
+)
+
+data class SectionEntity(
+    val id: Int?,
+    val title: String?,
+    val type: UiType?,
+    val url: String?,
+)
---
+++ model/src/main/java/com/wkylast/model/type/UiType.kt
@@ -0,0 +1,13 @@
+package com.wkylast.model.type
+
+enum class UiType {
+    HORIZONTAL,
+    VERTICAL,
+    GRID;
+
+    companion object {
+        fun from(type: String): UiType {
+            return entries.first { it.name == type }
+        }
+    }
+}
