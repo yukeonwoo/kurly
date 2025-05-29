9df2c2367ff8b189485f93ca5bcb9bb9aeaa6f4e
유건우 <kw.yu@birdview.kr>
2025-05-30 08:32:03 +0900

    uiState 추가

--- presentation/src/main/java/com/wkylast/presentation/goods/viewmodel/GoodsViewModel.kt
+++ presentation/src/main/java/com/wkylast/presentation/goods/viewmodel/GoodsViewModel.kt
@@ -2,11 +2,18 @@ package com.wkylast.presentation.goods.viewmodel
 
 import com.wkylast.presentation.base.MVIViewModel
 import com.wkylast.presentation.base.model.ViewIntent
+import com.wkylast.presentation.goods.state.GoodsUiState
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import javax.inject.Inject
 
 @HiltViewModel
 class GoodsViewModel @Inject constructor(): MVIViewModel<GoodsViewModel.Intent>() {
+
+    private val _uiState = MutableStateFlow(GoodsUiState())
+    val uiState = _uiState.asStateFlow()
+
 
     override fun dispatchIntent(intent: Intent) {
         TODO("Not yet implemented")
