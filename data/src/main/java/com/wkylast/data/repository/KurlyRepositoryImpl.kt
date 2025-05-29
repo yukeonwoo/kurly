04b2b8b4a6d85ef48377ccc1dca964e148647448
유건우 <kw.yu@birdview.kr>
2025-05-30 07:49:16 +0900

    코루틴 디스패처 추가

--- data/src/main/java/com/wkylast/data/repository/KurlyRepositoryImpl.kt
+++ data/src/main/java/com/wkylast/data/repository/KurlyRepositoryImpl.kt
@@ -3,11 +3,15 @@ package com.wkylast.data.repository
 import com.wkylast.domain.repository.KurlyRepository
 import com.wkylast.model.entity.ProductsEntity
 import com.wkylast.model.entity.SectionsEntity
+import com.wkylast.network.di.Dispatcher
+import com.wkylast.network.di.KurlyDispatchers
 import com.wkylast.network.remote.KurlyRemoteDataSource
+import kotlinx.coroutines.CoroutineDispatcher
 import javax.inject.Inject
 
 class KurlyRepositoryImpl @Inject constructor(
-    private val remoteDataSource: KurlyRemoteDataSource
+    private val remoteDataSource: KurlyRemoteDataSource,
+    @Dispatcher(KurlyDispatchers.IO) private val ioDispatcher: CoroutineDispatcher
 ): KurlyRepository {
 
     override suspend fun getSections(page: Int): SectionsEntity {
