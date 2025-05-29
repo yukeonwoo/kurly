1602fc2d9e026a539049ee4cd1e0ad83d8c87495
유건우 <kw.yu@birdview.kr>
2025-05-29 16:36:42 +0900

    hilt 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,8 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.dagger.hilt)
+    id("kotlin-kapt")
 }
 
 android {
@@ -46,6 +48,9 @@ dependencies {
     implementation(libs.androidx.ui)
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
+
+    implementation(libs.hilt.android)
+    kapt(libs.hilt.compiler)
 
     implementation(libs.androidx.material3)
     testImplementation(libs.junit)
--- app/src/main/java/com/wkylast/kurly/KurlyApplication.kt
+++ app/src/main/java/com/wkylast/kurly/KurlyApplication.kt
@@ -1,5 +1,7 @@
 package com.wkylast.kurly
 
 import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
 
+@HiltAndroidApp
 class KurlyApplication: Application()
