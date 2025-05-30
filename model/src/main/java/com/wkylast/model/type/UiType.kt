f9b53fa8e639a4c0cf460bbfe518be9b8e91e4b4
유건우 <kw.yu@birdview.kr>
2025-05-30 17:30:59 +0900

    UiType value 추가 및 from 로직 수정

--- model/src/main/java/com/wkylast/model/type/UiType.kt
+++ model/src/main/java/com/wkylast/model/type/UiType.kt
@@ -1,13 +1,13 @@
 package com.wkylast.model.type
 
-enum class UiType {
-    HORIZONTAL,
-    VERTICAL,
-    GRID;
+enum class UiType(val value: String) {
+    HORIZONTAL("horizontal"),
+    VERTICAL("vertical"),
+    GRID("grid");
 
     companion object {
         fun from(type: String?): UiType {
-            return entries.first { it.name == type }
+            return entries.first { it.value == type }
         }
     }
 }
