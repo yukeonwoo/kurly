385cfe97e94e3e2d8b90f79dea9705a9ba9b7487
유건우 <kw.yu@birdview.kr>
2025-05-30 10:39:04 +0900

    빌드를 위한 세팅 값 변경

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,6 +40,8 @@ android {
 
 dependencies {
     implementation(project(":presentation"))
+    implementation(project(":data"))
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
