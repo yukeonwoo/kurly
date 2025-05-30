1e47cdfe5944464ddbe71b739d1e865a8def312b
유건우 <kw.yu@birdview.kr>
2025-05-30 13:49:14 +0900

    SectionTitle Composable 추가

--- presentation/src/main/java/com/wkylast/presentation/goods/view/Sections.kt
+++ presentation/src/main/java/com/wkylast/presentation/goods/view/Sections.kt
@@ -1,12 +1,17 @@
 package com.wkylast.presentation.goods.view
 
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.snapshotFlow
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.wkylast.presentation.goods.state.SectionState
 import kotlinx.collections.immutable.ImmutableList
 import kotlinx.coroutines.flow.distinctUntilChanged
@@ -27,7 +32,9 @@ fun Sections(
            contentType = { it.uiContentType }
        ) { item ->
            when (item) {
-               is SectionState.Title -> Unit
+               is SectionState.Title -> {
+                   SectionTitle(item.title ?: "")
+               }
                is SectionState.Horizontal -> Unit
                is SectionState.Vertical -> Unit
                is SectionState.Grid -> Unit
@@ -51,4 +58,16 @@ fun Sections(
                 }
             }
     }
+}
+
+@Composable
+fun SectionTitle(
+    title: String,
+    modifier: Modifier = Modifier
+) {
+    Text(
+        text = title,
+        modifier = modifier.padding(16.dp),
+        style = MaterialTheme.typography.titleLarge
+    )
 }
