c367bbeb4db548a2a235d684524e1d6d66194037
유건우 <kw.yu@birdview.kr>
2025-06-01 08:17:37 +0900

    Log import 제거

--- presentation/src/main/java/com/wkylast/presentation/goods/view/GoodsScreen.kt
+++ presentation/src/main/java/com/wkylast/presentation/goods/view/GoodsScreen.kt
@@ -1,6 +1,5 @@
 package com.wkylast.presentation.goods.view
 
-import android.util.Log
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.ExperimentalMaterial3Api
--- presentation/src/main/java/com/wkylast/presentation/goods/view/Sections.kt
+++ presentation/src/main/java/com/wkylast/presentation/goods/view/Sections.kt
@@ -1,6 +1,5 @@
 package com.wkylast.presentation.goods.view
 
-import android.util.Log
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
